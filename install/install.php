e96ca20c019cba1c63e2a4937e29e1fa4fe6673a
Giuseppe Elefante <Giuseppe Elefante>
2016-11-24 12:01:45 +0100

    install script

--- install/install.php
+++ install/install.php
@@ -1,24 +1,30 @@
 <?php
-    require_once("functions.php");
+	require_once("../functions.php");
+    echo "Creo il database<br>";
     createDatabase();
+    echo "Aggiungo le tabelle del database<br>";
     createTables();
+    echo "Carico i comuni<br>";
     caricaComuni();
+
+	if(unlink("install.php"))
+        echo "install.php rimosso con successo<br>";
+    else
+        echo "<b>ATTENZIONE: DEVI RIMUOVERE IL FILE install.php MANUALMENTE</b><br>";
+        
 	echo "Fine";
 
     function createDatabase()
     {
-		echo "Creo il database<br>";
         //DROP Database
         //CREATE database
     }
     function createTables()
     {
-		echo "Aggiungo le tabelle<br>";
         //CREATE TABLES
     }
     function caricaComuni()
     {
-		echo "Carico i comuni<br>";
         $myfile = fopen("listacomuni.txt", "r") or die("File dei comuni assente!");
         while(!feof($myfile))
         {
@@ -35,7 +41,6 @@
     }
     function inserisciComune($istat, $comune, $prov, $prefisso, $cap, $codFis)
     {
-		echo "$istat $comune $prov $prefisso $cap $codFis <br>";
         $query = "INSERT INTO comune (istat, comune, provincia, prefisso, cap, codFiscale) VALUES (?,?,?,?,?,?)";
         $dbConn = dbConnect();
         $st = $dbConn->prepare($query);
